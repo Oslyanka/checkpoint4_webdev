a8842416672cd8f580c4bfaaff686c1be9828cd4
Your Name <you@example.com>
2023-09-11 14:47:00 -0300

    adicionei os 40 paragrafos

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,17 +3,21 @@ import { } from 'react'
 function Home(){
     return(
         <>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Maecenas vitae augue mauris. In gravida risus id leo auctor luctus.
-            Vestibulum interdum orci ac elit euismod, vestibulum accumsan tellus egestas.
-            Vestibulum in urna viverra tellus egestas tristique. Ut porta, elit nec pretium iaculis, nisi purus euismod leo, quis ultricies nibh ante sit amet nibh.
-            Nullam a lacus lobortis, ultrices mi sed, vehicula mauris.
-            Aliquam varius accumsan eros, ut sollicitudin purus efficitur non.
-            Curabitur egestas turpis at justo vestibulum dapibus. Vestibulum molestie elit urna, vitae convallis elit egestas ut.
-            Etiam imperdiet neque eu lectus tempor ultrices id nec massa. Phasellus id egestas lorem.
-            In dolor est, pharetra in tellus at, facilisis congue ex. Fusce et tincidunt nisi.
-            Vivamus vel enim arcu. Maecenas in ipsum vehicula ex euismod tempor quis quis ipsum.
-            Aenean volutpat velit id massa dapibus, non auctor ipsum sodales.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Maecenas vitae augue mauris.</p>
+        <p>In gravida risus id leo auctor luctus.</p>
+        <p>Vestibulum interdum orci ac elit euismod, vestibulum accumsan tellus egestas.</p>
+        <p>Vestibulum in urna viverra tellus egestas tristique.</p>
+        <p>Ut porta, elit nec pretium iaculis, nisi purus euismod leo, quis ultricies nibh ante sit amet nibh.</p>
+        <p>Nullam a lacus lobortis, ultrices mi sed, vehicula mauris.</p>
+        <p>Aliquam varius accumsan eros, ut sollicitudin purus efficitur non.</p>
+        <p>Curabitur egestas turpis at justo vestibulum dapibus.</p>
+        <p>Vestibulum molestie elit urna, vitae convallis elit egestas ut.</p>
+        <p>Etiam imperdiet neque eu lectus tempor ultrices id nec massa. Phasellus id egestas lorem.</p>
+        <p>In dolor est, pharetra in tellus at, facilisis congue ex. Fusce et tincidunt nisi.</p>
+        <p>Vivamus vel enim arcu.</p>
+        <p>Maecenas in ipsum vehicula ex euismod tempor quis quis ipsum.</p>
+        <p>Aenean volutpat velit id massa dapibus, non auctor ipsum sodales.</p>
         </>
     )
 }
