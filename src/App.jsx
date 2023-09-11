7a1af594e00b589abb8069f0952c8a21a5ebdb24
Your Name <you@example.com>
2023-09-11 14:11:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import { } from 'react'
+
+function Footer(){
+    return(
+        <>
+        <p>Celular: (85)98840-5555 </p>
+        <p>Email: rm55555@fiap.com.br</p>
+        </>
+    )
+}
+export default Footer
