e08b5ba78e2c16b760cf6212894ddc7b348d1df6
Your Name <you@example.com>
2023-09-11 14:32:34 -0300

    adicionei algumas coisas no home e no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { } from 'react'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Footer from './Footer'
+import Home from './Home'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+    
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import { } from 'react'
+
+function Home(){
+    return(
+        <>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Maecenas vitae augue mauris. In gravida risus id leo auctor luctus.
+            Vestibulum interdum orci ac elit euismod, vestibulum accumsan tellus egestas.
+            Vestibulum in urna viverra tellus egestas tristique. Ut porta, elit nec pretium iaculis, nisi purus euismod leo, quis ultricies nibh ante sit amet nibh.
+            Nullam a lacus lobortis, ultrices mi sed, vehicula mauris.
+            Aliquam varius accumsan eros, ut sollicitudin purus efficitur non.
+            Curabitur egestas turpis at justo vestibulum dapibus. Vestibulum molestie elit urna, vitae convallis elit egestas ut.
+            Etiam imperdiet neque eu lectus tempor ultrices id nec massa. Phasellus id egestas lorem.
+            In dolor est, pharetra in tellus at, facilisis congue ex. Fusce et tincidunt nisi.
+            Vivamus vel enim arcu. Maecenas in ipsum vehicula ex euismod tempor quis quis ipsum.
+            Aenean volutpat velit id massa dapibus, non auctor ipsum sodales.</p>
+        </>
+    )
+}
+export default Home
